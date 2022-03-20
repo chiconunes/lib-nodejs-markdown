587c4cc6488fb9b4317a181b3c36d7d836a08f29
chiconunes <fcn_jr@hotmail.com>
2022-03-20 20:54:23 -0300

    Feito juncao de array de url com hhtpCode do teste.. e realziado tratamento de erros em htt-val..

--- http-validacao.js
+++ http-validacao.js
@@ -1,21 +1,40 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const chalk = require('chalk');
+
+function manejaErros(erro) {
+    throw new Error(chalk.red(erro.message));    
+}
 
 async function checaStatus(arrayURLs) {
-    const arrayStatus = await Promise.all(arrayURLs.map(async url =>{
-        const res = await fetch(url)
-        return res.status;
-    }))
-    return arrayStatus;
+    try {
+        const arrayStatus = await Promise
+            .all(arrayURLs
+                .map(async url =>{
+                const res = await fetch(url)
+                return res.status;
+        }))
+        return arrayStatus;        
+    } catch (error) {
+        manejaErros(error);
+        
+    }
 }
 
 function geraArrayDeURLS(arrayLinks) {
-    return arrayLinks.map(ObjetoLink => Object.values(ObjetoLink).join())
+    return arrayLinks
+        .map(ObjetoLink => Object
+            .values(ObjetoLink).join())
 }
 
 async function validaUrls(arrayLinks) {
     const links =  geraArrayDeURLS(arrayLinks);
     const statusLinks = await checaStatus(links);
-    return statusLinks;
+
+    const resultado = arrayLinks.map((objeto,indice)=>({
+        ...objeto,
+        status: statusLinks[indice]
+    }))
+    return resultado;
 }
 
 module.exports=  validaUrls;
