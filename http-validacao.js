7d5b3d0618d285e702e19fc57e949dc6581469cb
chiconunes <fcn_jr@hotmail.com>
2022-03-20 20:24:21 -0300

    adicionado lib fetch para consumir http e checar o httpCode de cada URL

--- http-validacao.js
+++ http-validacao.js
@@ -1,9 +1,21 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+async function checaStatus(arrayURLs) {
+    const arrayStatus = await Promise.all(arrayURLs.map(async url =>{
+        const res = await fetch(url)
+        return res.status;
+    }))
+    return arrayStatus;
+}
+
 function geraArrayDeURLS(arrayLinks) {
     return arrayLinks.map(ObjetoLink => Object.values(ObjetoLink).join())
 }
 
-function validaUrls(arrayLinks) {
-    return geraArrayDeURLS(arrayLinks);
+async function validaUrls(arrayLinks) {
+    const links =  geraArrayDeURLS(arrayLinks);
+    const statusLinks = await checaStatus(links);
+    return statusLinks;
 }
 
-module.exports = validaUrls;
+module.exports=  validaUrls;
