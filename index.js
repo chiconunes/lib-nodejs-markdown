cdecf9a4b70f147f2fb3e50a8a8fa028fea35d95
chiconunes <fcn_jr@hotmail.com>
2022-03-20 16:41:03 -0300

    Preparando ambiente para validar os links

--- index.js
+++ index.js
@@ -39,3 +39,21 @@ async function pegaArquivo(caminhoDoArquivo) {
 // pegaArquivo('./arquivos/texto1.md');
 
 module.exports =  pegaArquivo;
+
+
+
+async function pegarArquivoDir(caminho) {
+  const caminhoAbsoluto = path.join(__dirname, '..', caminho);
+  const encoding = 'utf-8';
+  try {
+    const arquivos = await fs.promises.readdir(caminhoAbsoluto, { encoding });
+    const result = await Promise.all(arquivos.map(async (arquivo) => {
+      const localArquivo = `${caminhoAbsoluto}/${arquivo}`;
+      const texto = await fs.promises.readFile(localArquivo, encoding);
+      return extraiLinks(texto);
+    }));
+    return result;
+  } catch (erro) {
+    return trataErro(erro);
+  }
+ }
