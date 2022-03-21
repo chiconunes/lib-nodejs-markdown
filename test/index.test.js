50f96c30a8869771700c27c9709c627241787d54
chiconunes <fcn_jr@hotmail.com>
2022-03-20 22:23:28 -0300

    Adicionado arquivo JEST para testes e novos arquivos para apoiar o teste

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,20 @@
 const pegaArquivo = require('../index');
+const arrayResult = [
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+]
 
-test('1 - Deve ser uma função', () => {
-    expect(typeof pegaArquivo).toBe('function');
-});
+describe('pegaArquivo::',()=>{
+    it('1 - Deve ser uma função', () => {
+        expect(typeof pegaArquivo).toBe('function');
+    })
+    it('2 - Deve retornar um array com resultado', async () => {
+        const resultado = await pegaArquivo('C:\\@Programação\\NodeJS\\Estudos\\lib-nodejs-markdown\\test\\arquivos\\texto1link.md')
+        expect(resultado).toEqual(arrayResult);
+    })
+
+})
+
+
+
